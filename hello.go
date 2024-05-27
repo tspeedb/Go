dde5408a197da5c761ebb446cf6fe5fcc2dd3838
Tommy <thomassbennett@gmail.com>
2024-05-27 11:34:47 -0400

    Updated for Arrays and Slices

--- hello.go
+++ hello.go
@@ -1,13 +1,19 @@
 package main
 
-import "fmt"
-
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
+	if language == "French" {
+		return frenchHelloPrefix + name
+	}
 	return englishHelloPrefix + name
-}
-
-func main() {
-	fmt.Println(Hello("world"))
 }
