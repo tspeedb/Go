904215d4ae36fec969a1e592f81f710e01e78709
Tommy <thomassbennett@gmail.com>
2024-06-01 22:53:22 -0400

    Created tests to learn abt structs and interfaces

---
+++ StructMthdInterface/shapes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"math"
+)
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func (r Rectangle) Perimeter() float64 {
+	return (2 * r.Width) + (2 * r.Height)
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
