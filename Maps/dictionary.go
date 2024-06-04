6f7aa8d75e8dd3d99e0448f56582fba48aecb7f1
Tommy <thomassbennett@gmail.com>
2024-06-03 22:51:08 -0400

    Added delete

--- Maps/dictionary.go
+++ Maps/dictionary.go
@@ -49,6 +49,10 @@ func (d Dictionary) Update(word, definition string) error {
 	return nil
 }
 
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
+}
+
 func (e DictionaryErr) Error() string {
 	return string(e)
 }
